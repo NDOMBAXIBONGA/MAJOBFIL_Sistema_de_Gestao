c5454e032570e210c5522a25b60f7fb30a8fee94
Ndombaxi_Bonga <jobdealmeida016@gmail.com>
2026-03-18 20:27:37 +0000

    new database

--- majobfil/settings.py
+++ majobfil/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
-#import dj_database_url
+import dj_database_url
 
 # Configurações de segurança
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 SECRET_KEY = os.environ.get('SECRET_KEY', 'sua-chave-local-aqui')
 
 ALLOWED_HOSTS = [
@@ -100,24 +101,21 @@ WSGI_APPLICATION = 'majobfil.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.environ.get('PGDATABASE'),
-#        'USER': os.environ.get('PGUSER'),
-#        'PASSWORD': os.environ.get('PGPASSWORD'),
-#        'HOST': os.environ.get('PGHOST'),
-#        'PORT': os.environ.get('PGPORT', '5432'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:FXCXRkozcXVYOTXNXpgjXXECikZgviaw@interchange.proxy.rlwy.net:24064/railway',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
